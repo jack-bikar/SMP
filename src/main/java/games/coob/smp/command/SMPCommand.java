7a6bd31024e1ff3598fbef1b67d3ccdfb1b6a3cf
JackOUT <jack.bikar@gmail.com>
2026-01-30 00:41:39 +0100

    Updated commands

--- src/main/java/games/coob/smp/command/SMPCommand.java
+++ src/main/java/games/coob/smp/command/SMPCommand.java
@@ -19,41 +19,32 @@ public class SMPCommand implements CommandExecutor, TabCompleter {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		// Check permission (plugin.yml handles this, but double-check)
-		if (!sender.hasPermission("smp.command.help") && !sender.isOp()) {
-			ColorUtil.sendMessage(sender, "&cYou don't have permission to use this command.");
-			return true;
-		}
-
 		// Send header
 		ColorUtil.sendMessage(sender, "&6&l=== SMP Plugin Help ===");
 		ColorUtil.sendMessage(sender, "");
-		
+
 		// Plugin info
 		ColorUtil.sendMessage(sender, "&ePlugin Version: &f1.0.0");
 		ColorUtil.sendMessage(sender, "&eAuthor: &fJackOUT");
 		ColorUtil.sendMessage(sender, "");
-		
+
 		// Commands
 		ColorUtil.sendMessage(sender, "&6&lCommands:");
 		ColorUtil.sendMessage(sender, "");
-		
+		ColorUtil.sendMessage(sender, "&e/smp &7- Show this help (all players)");
+		ColorUtil.sendMessage(sender, "");
 		ColorUtil.sendMessage(sender, "&e/spawn &7- Teleport to spawn");
 		ColorUtil.sendMessage(sender, "&7  &7/spawn locate &7- Show spawn coordinates");
 		ColorUtil.sendMessage(sender, "&7  &7/spawn set &7- Set spawn to your location");
 		ColorUtil.sendMessage(sender, "");
-		
-		ColorUtil.sendMessage(sender, "&e/inv <inv|enderchest|armour|clear> <player> &7- Edit player inventories");
-		ColorUtil.sendMessage(sender, "&7  &7/inv inv <player> &7- View/edit player inventory");
-		ColorUtil.sendMessage(sender, "&7  &7/inv enderchest <player> &7- View/edit player ender chest");
-		ColorUtil.sendMessage(sender, "&7  &7/inv armour <player> &7- View/edit player armor");
-		ColorUtil.sendMessage(sender, "&7  &7/inv clear <player> &7- Clear player inventory");
+		ColorUtil.sendMessage(sender, "&e/tracking &7- Locator bar tracking (all players)");
+		ColorUtil.sendMessage(sender, "&7  &7/tracking menu &7- Open locator tracking menu");
+		ColorUtil.sendMessage(sender, "&7  &7/tracking cancel &7- Cancel current tracking");
+		ColorUtil.sendMessage(sender, "&7  &7/tracking give [player] &7- Give locator tracker (built-in, OP)");
 		ColorUtil.sendMessage(sender, "");
-		ColorUtil.sendMessage(sender, "&e/tracking menu &7- Open locator tracking menu");
-		ColorUtil.sendMessage(sender, "&e/tracking cancel &7- Cancel current tracking");
-		ColorUtil.sendMessage(sender, "&e/tracking give [player] &7- Give locator tracker (built-in)");
+		ColorUtil.sendMessage(sender, "&e/inv <inv|enderchest|armour|clear> <player> &7- Edit player inventories (OP)");
 		ColorUtil.sendMessage(sender, "");
-		
+
 		// Features
 		ColorUtil.sendMessage(sender, "&6&lFeatures:");
 		ColorUtil.sendMessage(sender, "");
@@ -62,17 +53,18 @@ public class SMPCommand implements CommandExecutor, TabCompleter {
 		ColorUtil.sendMessage(sender, "&7- &eProjectile Effects &7- Particle trails and knockback for projectiles");
 		ColorUtil.sendMessage(sender, "&7- &eCombat System &7- Combat logging prevention");
 		ColorUtil.sendMessage(sender, "");
-		
+
+		// Permissions (only restricted commands)
 		ColorUtil.sendMessage(sender, "&6&lPermissions:");
 		ColorUtil.sendMessage(sender, "");
 		ColorUtil.sendMessage(sender, "&7- &esmp.command.spawn &7- Use /spawn command");
 		ColorUtil.sendMessage(sender, "&7- &esmp.command.inv &7- Use /inv command");
-		ColorUtil.sendMessage(sender, "&7- &esmp.command.help &7- Use /smp help command");
-		ColorUtil.sendMessage(sender, "&7- &esmp.command.tracking &7- Use /tracking command");
+		ColorUtil.sendMessage(sender, "&7- &esmp.command.tracking.menu &7- Open /tracking menu");
+		ColorUtil.sendMessage(sender, "&7- &esmp.command.tracking.give &7- Use /tracking give");
 		ColorUtil.sendMessage(sender, "");
-		
+
 		ColorUtil.sendMessage(sender, "&6&l=== === ===");
-		
+
 		return true;
 	}
 
